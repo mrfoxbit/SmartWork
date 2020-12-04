488eedad58c2d5f19f43cef0b3807b5165f45252
hoangtuit <phamoangtu@gmail.com>
2020-12-04 23:18:48 +0700

    Start Project

---
+++ index.js
@@ -0,0 +1,45 @@
+// express
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+app.listen(3000, () => console.log("Server started."));
+
+// body-parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// Define routes
+
+app.get("/", function(req, res){
+
+    res.render("index");
+
+});
+
+app.get("/login", function(req, res){
+
+    res.render("login");
+
+});
+
+app.get("/logout", function(req, res){
+
+    res.redirect("login");
+
+});
+
+app.get("/inbox", function(req, res){
+
+    res.render("inbox");
+
+});
+
+app.get("/administration", function(req, res){
+
+    res.render("index");
+
+});
---
+++ public/assets/js/pages/my-script.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!******************************************!*\
+  !*** ../demo1/src/js/pages/my-script.js ***!
+  \******************************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+
+/******/ })()
+;
